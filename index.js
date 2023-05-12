37477b1ebe6cfafc43af7f390430586bd17c8b2f
Damilola Michael Ige <damilola@aeeiee.com>
2023-05-12 22:39:23 +0100

    adds css styling to swagger document

--- index.js
+++ index.js
@@ -20,9 +20,11 @@ const apiRateLimiter = rateLimit({
     message: 'Too many requests from this IP. Please try again after 15 minutes.',
 });
 
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
 app.use(cors());
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { customCssUrl: CSS_URL }));
 app.use(morgan('dev'));
 
 // Apply rate limiter to all API routes
